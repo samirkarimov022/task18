cb736ffca53878347ebb70154417a1aa165f4bd2
samirkarimov022 <samir.karimov0022@gmail.com>
2024-04-16 03:39:29 +0400

    add task

---
+++ Exception/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Test test = new Test();
+            test[0] = 1;
+            test[1] = 2;
+            test[11] = 3;
+            //for (int i = 0;i <test.Length;i++)
+            //{
+            //    Console.WriteLine(test[i]);
+            //}
+            foreach (var item in typeof(Test).GetProperties())
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+    class Test
+    {
+
+        public int[] arr { get; set; }
+        public int Length => arr.Length;
+        public Test()
+        {
+            arr = new int[10];
+        }
+        public int this[int i]
+        {
+            get { return arr[i]; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("0 dan kicikdir");
+                }
+                if (i >= arr.Length)
+                {
+                    throw new IndexOutOfRangeException("uzunluqu coxdur");
+                }
+                arr[i] = value;
+            }
+        }
+
+    }
+
+}
